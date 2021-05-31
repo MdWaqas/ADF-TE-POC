210ff6f31fc737ba7252500c2c1de1e98cccbb94
Waqas Idrees <waqas.idrees@confiz.com>
2021-05-31 19:09:24 +0500

    Enabled swagger for production.

--- ADF-TE-POC/Startup.cs
+++ ADF-TE-POC/Startup.cs
@@ -65,9 +65,11 @@ namespace ADF_TE_POC
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ADF_TE_POC v1"));
+                
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ADF_TE_POC v1"));
 
             app.UseHttpsRedirection();
 
